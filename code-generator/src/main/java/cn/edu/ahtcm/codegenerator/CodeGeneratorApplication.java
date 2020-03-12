194121b469e6082be849129f193f87bf7d84d192
liuyu12 <1454473139@qq.com>
2020-03-12 16:44:19 +0800

    去除数据库配置

--- code-generator/src/main/java/cn/edu/ahtcm/codegenerator/CodeGeneratorApplication.java
+++ code-generator/src/main/java/cn/edu/ahtcm/codegenerator/CodeGeneratorApplication.java
@@ -2,14 +2,16 @@ package cn.edu.ahtcm.codegenerator;
 
 import cn.edu.ahtcm.webcommon.configure.WebMvcConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * @author liuyu
  */
 @Import({WebMvcConfigure.class})
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CodeGeneratorApplication {
 
     public static void main(String[] args) {

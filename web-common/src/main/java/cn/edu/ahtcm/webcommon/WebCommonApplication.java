f7101f595c63aaef7bd43ec304ff1fe9aaa69590
liuyu12 <1454473139@qq.com>
2020-03-12 17:10:08 +0800

    项目基础架构

---
+++ web-common/src/main/java/cn/edu/ahtcm/webcommon/WebCommonApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.ahtcm.webcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class WebCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCommonApplication.class, args);
+    }
+
+}
